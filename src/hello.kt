2c580cffbe09943ca46dc28f9b4eb72a8c326844
Lakshmi Narayanan Narasimhan <grabyourfreedom@gmail.com>
2018-04-30 12:33:13 +0530

    First Kotlin program

--- src/hello.kt
+++ src/hello.kt
@@ -1,3 +1,8 @@
+/* This is a simple program to demonstrate the first
+ * program in Kotlin. This program is a traditional
+ * Hello World program that prints the words
+ * "Hello, World !!!
+ */
 fun main(args : Array<String>) {
-    println("Hello, World")
+    println("Hello, World !!!")
 }
